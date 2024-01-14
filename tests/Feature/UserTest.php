d8f60d4c1ad608ec4bc65f0b7159384a98eb689d
Lynt <lintangkusuma17@gmail.com>
2024-01-14 09:24:36 +0700

    Register User API

---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class UserTest extends TestCase
+{
+
+    public function testRegisterSuccess()
+    {
+        $this->post('/api/users',[
+            "username" => "lynnn",
+            "password" => "ndaktau",
+            "name" => "Lynnn 1702"
+        ])->assertStatus(201)
+        ->assertJson([
+            "data" => [
+                'username' => 'lynnn',
+                'name' => 'Lynnn 1702'
+            ]
+        ]);
+
+
+    }
+
+    public function testRegisterFailed()
+    {
+        $this->post('/api/users',[
+            "username" => "",
+            "password" => "",
+            "name" => " "
+        ])->assertStatus(400)
+            ->assertJson([
+                "errors" => [
+                    "username" => [
+                        "The username field is required."
+                    ],
+                    "password" => [
+                        "The password field is required."
+                    ],
+                    "name" => [
+                        "The name field is required."
+                    ]
+                ]
+            ]);
+    }
+
+    public function testRegisterUsernameAlreadyExists()
+    {
+        $this->testRegisterSuccess();
+        $this->post('/api/users',[
+            "username" => "lynnn",
+            "password" => "ndaktau",
+            "name" => "Lynnn 1702"
+        ])->assertStatus(400)
+            ->assertJson([
+                "errors" => [
+                    'username' => [
+                        "username already registered"
+                    ]
+                ]
+            ]);
+    }
+}
